1f6a51faca81812d61777da55581f4822e6f7e81
XPandora <330281987@qq.com>
2017-11-19 14:53:20 +0800

    搜索框功能完成
    完善了一下页面，并完善了搜索框，支持点击跳转

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,8 +1,69 @@
-
 new Vue({
-    el: '#main',
+    el: '#search',
     data: {
         searchString: "",
+        articles: [
+            {
+                "title": "The_Donald",
+                "url": "https://www.reddit.com/r/The_Donald/",
+            },
+            {
+                "title": "Politics",
+                "url": "https://www.reddit.com/r/politics/",
+            },
+            {
+                "title": "Gaming",
+                "url": "https://www.reddit.com/r/gaming/",
+            },
+            {
+                "title": "Funny",
+                "url": "https://www.reddit.com/r/funny/",
+            },
+            {
+                "title": "NBA",
+                "url": "https://www.reddit.com/r/nba/",
+            },
+            {
+                "title": "Videos",
+                "url": "https://www.reddit.com/r/videos/",
+            },
+            {
+                "title": "Today I learned",
+                "url": "https://www.reddit.com/r/todayilearned/",
+            },
+            {
+                "title": "Soccer",
+                "url": "https://www.reddit.com/r/soccer/",
+            },
+            {
+                "title": "World News",
+                "url": "https://www.reddit.com/r/worldnews/",
+            },
+            {
+                "title": "Movies",
+                "url": "https://www.reddit.com/r/movies/",
+            },
+            {
+                "title": "Aww",
+                "url": "https://www.reddit.com/r/aww/",
+            },
+            {
+                "title": "Gifs",
+                "url": "https://www.reddit.com/r/gifs/",
+            },
+            {
+                "title": "Television",
+                "url": "https://www.reddit.com/r/television/",
+            },
+            {
+                "title": "Black People Twitter",
+                "url": "https://www.reddit.com/r/BlackPeopleTwitter/",
+            },
+            {
+                "title": "League of Legends",
+                "url": "https://www.reddit.com/r/leagueoflegends/",
+            }
+        ]
 
     },
     computed: {
@@ -11,7 +72,7 @@ new Vue({
                 searchString = this.searchString;
 
             if(!searchString){
-                return articles_array;
+                return [];
             }
 
             searchString = searchString.trim().toLowerCase();
