e138e7a22988692a0787a381cd2d5d84044f605e
XPandora <330281987@qq.com>
2017-11-19 10:14:13 +0800

    做了一定的改进，添加了搜索框

---
+++ assets/js/search.js
@@ -0,0 +1,27 @@
+
+new Vue({
+    el: '#main',
+    data: {
+        searchString: "",
+
+    },
+    computed: {
+        filteredArticles: function () {
+            var articles_array = this.articles,
+                searchString = this.searchString;
+
+            if(!searchString){
+                return articles_array;
+            }
+
+            searchString = searchString.trim().toLowerCase();
+
+            articles_array = articles_array.filter(function(item){
+                if(item.title.toLowerCase().indexOf(searchString) !== -1){
+                    return item;
+                }
+            })
+            return articles_array;;
+        }
+    }
+});
